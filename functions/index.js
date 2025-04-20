13e8edd21afe7234ea1e51792b5e0680196e8a2e
Laznier Mederos <laznier@yahoo.com>
2025-04-19 19:20:44 -0700

    .

--- functions/index.js
+++ functions/index.js
@@ -1,50 +1,45 @@
-// functions/index.js
-// functions/index.js
-const functions = require('firebase-functions');
-const admin     = require('firebase‑admin');
+const {onSchedule} = require("firebase-functions/v2/scheduler");
+const admin = require("firebase-admin");
 
-admin.initializeApp();   // ← now defined again
+admin.initializeApp();
 const db = admin.database();
 
+exports.downgradeExpiredTrials = onSchedule(
+    {
+      schedule: "every 1 hours",
+      timeZone: "Etc/UTC",
+    },
+    async () => {
+      const now = Date.now();
+      const usersRef = db.ref("users");
+      const snap = await usersRef.once("value");
+      const updates = {};
 
+      snap.forEach((childSnap) => {
+        const data = childSnap.val();
+        const uid = childSnap.key;
 
-
-exports.downgradeExpiredTrials = functions
-  .pubsub
-  .schedule('every 1 hours')
-  .timeZone('Etc/UTC')
-  .onRun(async (context) => {
-    const now      = Date.now();
-    const usersRef = db.ref('users');
-    const snap     = await usersRef.once('value');
-    const updates  = {};
-
-    snap.forEach(childSnap => {
-      const data = childSnap.val();
-      const uid  = childSnap.key;
-
-      // if they were on trial and 24h passed, downgrade
-      if (
-        data.role === 'premium' &&
+        if (
+          data.role === "premium" &&
         data.trialStart &&
         now > data.trialStart + 24 * 60 * 60 * 1000
-      ) {
-        updates[`${uid}/role`] = 'basic';
-      }
+        ) {
+          updates[`${uid}/role`] = "basic";
+        }
 
-      // if they had a paid subscription that’s now past expiry, downgrade
-      if (
-        data.role === 'premium' &&
+        if (
+          data.role === "premium" &&
         data.subscriptionExpiry &&
         now > data.subscriptionExpiry
-      ) {
-        updates[`${uid}/role`] = 'basic';
+        ) {
+          updates[`${uid}/role`] = "basic";
+        }
+      });
+
+      if (Object.keys(updates).length) {
+        await usersRef.update(updates);
       }
-    });
 
-    if (Object.keys(updates).length) {
-      await usersRef.update(updates);
-    }
-
-    return null;
-  });
+      return null;
+    },
+);
