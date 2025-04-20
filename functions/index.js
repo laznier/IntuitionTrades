e41541d1131083f1b4ff700721f5efe6b3a65600
Laznier Mederos <laznier@yahoo.com>
2025-04-19 17:21:43 -0700

    .

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,38 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+// functions/index.js
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions");
+const admin     = require("firebase-admin");
+admin.initializeApp();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// every hour, check for expired trials
+exports.downgradeExpiredTrials = functions.pubsub
+  .schedule("every 60 minutes")
+  .onRun(async (_context) => {
+    const db   = admin.database();
+    const now  = Date.now();
+    const usersSnap = await db.ref("users").once("value");
+    const updates   = {};
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    usersSnap.forEach(userSnap => {
+      const u = userSnap.val();
+      // if they used a trial, are still marked 'premium', and 24h has passed:
+      if (
+        u.trialUsed === true &&
+        u.role === "premium" &&
+        typeof u.trialStart === "number" &&
+        u.trialStart + 24 * 3600 * 1000 <= now
+      ) {
+        updates[`${userSnap.key}/role`] = "basic";
+      }
+    });
+
+    if (Object.keys(updates).length) {
+      await db.ref("users").update(updates);
+      console.log(
+        `Downgraded ${Object.keys(updates).length} user(s) from premium→basic.`
+      );
+    } else {
+      console.log("No expired trials at this time.");
+    }
+    return null;
+  });
